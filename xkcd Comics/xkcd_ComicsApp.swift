e55513c60f3014d6d341134c111085c43cdc21ab
Cynthia Wang <CynthiaWang7979@hotmail.com>
2021-07-30 01:19:53 +0800

    Initial Commit

---
+++ xkcd Comics/xkcd_ComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  xkcd_ComicsApp.swift
+//  xkcd Comics
+//
+//  Created by Xia He on 2021/7/30.
+//
+
+import SwiftUI
+
+@main
+struct xkcd_ComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
